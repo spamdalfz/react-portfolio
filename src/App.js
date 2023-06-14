bc474f16ca1d51e88dfcb2f9bc93f96d0c6f53c9
spamdalfz <coreyvasser@gmail.com>
2023-06-13 17:22:04 -0700

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
@@ -8,9 +8,14 @@ import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
 function App() {
+    const [activeTab, setActiveTab] = useState('/about');
+
     return (
         <Router>
-            <Header />
+            <Helmet>
+                <title>Portfolio</title>
+            </Helmet>
+            <Header activeTab={activeTab} setActiveTab={setActiveTab} />
             <Switch>
                 <Route path="/about" component={AboutMe} />
                 <Route path="/portfolio" component={Portfolio} />
