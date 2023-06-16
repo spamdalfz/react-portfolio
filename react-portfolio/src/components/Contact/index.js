4d954274c6a9a7d4510c2ab2b172272630f391ec
spamdalfz <coreyvasser@gmail.com>
2023-06-15 23:06:54 -0700

    new file:   react-portfolio/public/resume/Corey_Vasser_Resume_2023.pdf 	modified:   react-portfolio/src/components/Contact/index.js 	modified:   react-portfolio/src/components/Resume/index.js

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <section>
-            <h1>Contact</h1>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" required />
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" required />
-                </label>
-                <label>
-                    Message:
-                    <textarea name="message" required />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+        <section className="bg-gray-800 text-white p-5">
+            <h1 className="text-4xl font-bold mb-10 text-center">Contact</h1>
+            <div className="max-w-md mx-auto">
+                <form>
+                    <label className="block">
+                        <span className="text-xl mb-2">Name:</span>
+                        <input type="text" name="name" required className="w-full px-3 py-2 rounded-lg border border-gray-600 focus:outline-none focus:border-blue-500" />
+                    </label>
+                    <label className="block mt-5">
+                        <span className="text-xl mb-2">Email:</span>
+                        <input type="email" name="email" required className="w-full px-3 py-2 rounded-lg border border-gray-600 focus:outline-none focus:border-blue-500" />
+                    </label>
+                    <label className="block mt-5">
+                        <span className="text-xl mb-2">Message:</span>
+                        <textarea name="message" required className="w-full px-3 py-2 rounded-lg border border-gray-600 focus:outline-none focus:border-blue-500 h-32" />
+                    </label>
+                    <input type="submit" value="Submit" className="block mt-5 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg cursor-pointer" />
+                </form>
+            </div>
         </section>
     )
 }
--- react-portfolio/src/components/Resume/index.js
+++ react-portfolio/src/components/Resume/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Resume = () => {
+    const resumePath = process.env.PUBLIC_URL + '/resume/Corey_Vasser_Resume_2023.pdf';
     return (
-        <div>
-            <h1>Resume</h1>
+        <div className="bg-gray-800 text-white p-5">
+            <h1 className="text-4xl font-bold mb-10 text-center">Resume</h1>
+            <div className="max-w-md mx-auto">
+                <embed src={resumePath} type="application/pdf" width="100%" height="600px" className="mb-10" />
+                <a href={resumePath} download className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg cursor-pointer">Download Resume</a>
+            </div>
         </div>
     );
 }
