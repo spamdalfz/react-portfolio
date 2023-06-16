c6e43fb725dac5c98d733a725535a27c40ba9411
spamdalfz <coreyvasser@gmail.com>
2023-06-15 17:52:41 -0700

    modified:   package.json 	modified:   src/App.js 	new file:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Header from "./components/Header";
-import AboutMe from "./components/AboutMe";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume";
-import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import Header from "./components/Header/Header";
+import AboutMe from "./components/AboutMe/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Resume from "./components/Resume/Resume";
+import Footer from "./components/Footer/Footer";
 import { Helmet } from "react-helmet";
 
 const App = () => {
     const [activeTab, setActiveTab] = useState('/about');
+
+    // define useNavigate to enable redirection
+    const navigate = useNavigate();
+
+    // useEffect hook to redirect to "/about" on initial render
+    useEffect(() => {
+        navigate("/about");
+    }, [navigate]);
 
     return (
         <Router>
@@ -17,13 +25,12 @@ const App = () => {
                 <title>Portfolio</title>
             </Helmet>
             <Header activeTab={activeTab} setActiveTab={setActiveTab} />
-            <Switch>
-                <Route path="/about" component={AboutMe} />
-                <Route path="/portfolio" component={Portfolio} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/resume" component={Resume} />
-                <Redirect from="/" to="/about" />
-            </Switch>
+            <Routes>
+                <Route path="/about" element={<AboutMe />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes>
             <Footer />
         </Router>
     );
