9bbad09a98571721801b092890d0d0115293ddab
spamdalfz <coreyvasser@gmail.com>
2023-06-16 01:50:19 -0700

    edited styles on about me page

--- react-portfolio/src/components/Header/index.js
+++ react-portfolio/src/components/Header/index.js
@@ -18,7 +18,7 @@ const Header = ({ activeTab, setActiveTab }) => {
             <nav className="flex justify-around">
                 <NavLink
                     to="/about"
-                    className={`${activeTab === '/about' ? 'text-lg font-bold' : 'text-lg'} hover:text-blue-500`}
+                    className={`text-xl ${activeTab === '/about' ? 'underline text-teal-500' : ''} hover:text-blue-500`}
                     onClick={() => handleTabChange('/about')}
                 >
                     About Me
@@ -26,21 +26,21 @@ const Header = ({ activeTab, setActiveTab }) => {
 
                 <NavLink
                     to="/portfolio"
-                    className={`${activeTab === '/portfolio' ? 'text-lg font-bold' : 'text-lg'} hover:text-blue-500`}
+                    className={`text-xl ${activeTab === '/portfolio' ? 'underline text-teal-500' : ''} hover:text-blue-500`}
                     onClick={() => handleTabChange('/portfolio')}
                 >
                     Portfolio
                 </NavLink>
                 <NavLink
                     to="/contact"
-                    className={`${activeTab === '/contact' ? 'text-lg font-bold' : 'text-lg'} hover:text-blue-500`}
+                    className={`text-xl ${activeTab === '/contact' ? 'underline text-teal-500' : ''} hover:text-blue-500`}
                     onClick={() => handleTabChange('/contact')}
                 >
                     Contact
                 </NavLink>
                 <NavLink
                     to="/resume"
-                    className={`${activeTab === '/resume' ? 'text-lg font-bold' : 'text-lg'} hover:text-blue-500`}
+                    className={`text-xl ${activeTab === '/resume' ? 'underline text-teal-500' : ''} hover:text-blue-500`}
                     onClick={() => handleTabChange('/resume')}
                 >
                     Resume
