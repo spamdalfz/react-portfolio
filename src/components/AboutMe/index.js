08b0df27fd2038f836063e77068531a4425abd2f
spamdalfz <coreyvasser@gmail.com>
2023-06-20 02:33:51 -0700

    adjusted profile pic

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,9 +3,13 @@ import profilePic from './images/profile-pic.jpg';
 
 const AboutMe = () => {
     return (
-        <div className="flex flex-col md:flex-row justify-center bg-gray-800 text-blue-400 min-h-screen py-10 px-4 md:px-20 font-josefin">
-            <img src={profilePic} alt="Corey Vasser" className="h-32 w-32 rounded-full mx-auto md:mx-0 shadow-lg md:mb-0 md:mr-10" />
-            <div className="text-center md:text-left space-y-4 w-full md:w-1/2">
+        <div className="flex flex-col justify-center items-center bg-gray-800 text-blue-400 min-h-screen py-10 px-4 md:px-20 font-josefin">
+            <div className="flex flex-col items-center justify-center w-full">
+                <img src={profilePic} alt="Corey Vasser" className="h-32  rounded-full mx-auto shadow-lg mb-4" />
+                <h1 className="text-3xl md:text-5xl font-bold my-4 text-center ">Corey Vasser</h1>
+                <h2 className="text-xl md:text-2xl font-semibold text-center mb-10">Web Developer</h2>
+            </div>
+            <div className="flex flex-col justify-center text-center md:text-left space-y-4 w-full md:w-2/3 md:mx-auto">
                 <h1 className="text-3xl md:text-5xl font-bold mb-4">About Me</h1>
                 <p className="text-lg md:text-xl">Hello! I'm Corey 👋, a dedicated Web Developer based in the beautiful Pacific Northwest. I hold a strong passion for computer science, with a particular focus on full-stack development and mastery of JavaScript.</p>
                 <p className="text-lg md:text-xl">I pride myself on being more than just a developer. I am a problem-solver at heart, always eager to dive into challenging situations and find efficient, creative solutions. I consider every problem as an opportunity to learn, grow, and refine my skills.</p>
