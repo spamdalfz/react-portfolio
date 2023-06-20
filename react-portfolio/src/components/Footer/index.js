22204b96cc2144eb1c6c0e65ab75055a2b189329
spamdalfz <coreyvasser@gmail.com>
2023-06-19 20:54:15 -0700

    added links and icons to footer

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <footer className="bg-black text-white text-center py-4">
             <h1 className="font-bold text-xl mb-2">Corey Vasser</h1>
+            <div className="mb-4">
+                <a href="https://github.com/spamdalfz" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white m-4">
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+                <a href="https://www.linkedin.com/in/corey-vasser/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white m-4">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </a>
+            </div>
             <p className="text-sm">&copy; {new Date().getFullYear()} Corey Vasser</p>
         </footer>
     );
 }
-
 export default Footer;
