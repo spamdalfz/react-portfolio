c5a313ed711ea9630928a5588639d573d0d8b51b
spamdalfz <coreyvasser@gmail.com>
2023-08-02 00:47:27 -0700

    modified:   .gitignore modified:   src/components/Contact/index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            e.target,
+            process.env.REACT_APP_EMAILJS_USER_ID
+        )
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
     return (
         <section className="bg-gray-800 text-blue-400 p-5  font-josefin min-h-screen">
             <h1 className="text-4xl font-bold mb-10 text-center">Contact</h1>
             <div className="max-w-md mx-auto">
-                <form>
+                <form onSubmit={sendEmail}>
                     <label className="block">
                         <span className="text-xl mb-2">Name:</span>
                         <input type="text" name="name" required className="w-full px-3 py-2 rounded-lg border border-gray-600 focus:outline-none focus:border-blue-500" />
